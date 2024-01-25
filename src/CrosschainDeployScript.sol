ea929d57bbc6619187a0372fedaaefb85f7c06ff
Vinay Keerthi <11478411+stonecharioteer@users.noreply.github.com>
2024-01-25 18:10:25 +0530

    feat(wip): track deployment targets using an array and store the available deployment targets as a mapping for easier use

--- src/CrosschainDeployScript.sol
+++ src/CrosschainDeployScript.sol
@@ -14,6 +14,26 @@ contract CrosschainDeployScript is Script {
     // this address is the same across all chains
     address private constant CROSS_CHAIN_DEPLOY_CONTRACT_ADDRESS = 0x85d62AD850B322152BF4ad9147bfBF097DA42217;
 
+    // given a string, obtain the domain ID;
+    // https://www.notion.so/chainsafe/Testnet-deployment-0483991cf1ac481593d37baf8d48712a
+
+    mapping(string => uint8) private _stringToDeploymentNetwork;
+    // store the domain IDs
+    uint8[] private _deploymentNetworks;
+
+    constructor() {
+        _stringToDeploymentNetwork["goerli"] = 1;
+        _stringToDeploymentNetwork["sepolia"] = 2;
+        _stringToDeploymentNetwork["rhala"] = 3; // unimplemented as of today;
+        _stringToDeploymentNetwork["base-goerli"] = 4; // unimplemented as of today;
+        _stringToDeploymentNetwork["cronos-testnet"] = 5;
+        _stringToDeploymentNetwork["holesky"] = 6;
+        _stringToDeploymentNetwork["mumbai"] = 7;
+        _stringToDeploymentNetwork["arbitrum-sepolia"] = 8;
+        _stringToDeploymentNetwork["gnosis-chaido"] = 9;
+        _stringToDeploymentNetwork["holesky"] = 6;
+    }
+
     /**
      * @notice this function takes in the contract string, in the form that
      * @notice `forge`'s `getCode` takes it, along with some other parameters and passes
@@ -32,20 +52,24 @@ contract CrosschainDeployScript is Script {
      *   and the `callData` and `bytesCode` are extracted from it.
      *   and the contract is deployed on the other chains.
      */
-    function deployContractOnOtherChains(
+    function deploy(
         string calldata contractString,
         uint256 gasLimit,
         bytes32 salt,
         bool isUniquePerChain,
         bytes[] memory constructorArgs,
-        bytes[] memory initDatas,
-        uint8[] memory destinationDomainIDs
-    ) public payable {
+        bytes[] memory initDatas
+    )
+        // uint8[] memory destinationDomainIDs
+        public
+        payable
+    {
+        require(deploymentNetworks.length > 0, "Need to add deployment targets. Use `addDeploymentTarget` first");
         // We use the contractString to get the bytecode of the contract,
         // reference: https://book.getfoundry.sh/cheatcodes/get-code
         bytes memory deployByteCode = vm.getCode(contractString);
         uint256[] memory fees = ICrosschainDeployAdapter(CROSS_CHAIN_DEPLOY_CONTRACT_ADDRESS).calculateDeployFee(
-            deployByteCode, gasLimit, salt, isUniquePerChain, constructorArgs, initDatas, destinationDomainIDs
+            deployByteCode, gasLimit, salt, isUniquePerChain, constructorArgs, initDatas, _deploymentNetworks
         );
         uint256 totalFee;
         uint256 feesArrayLength = fees.length;
@@ -58,7 +82,30 @@ contract CrosschainDeployScript is Script {
         }
 
         ICrosschainDeployAdapter(CROSS_CHAIN_DEPLOY_CONTRACT_ADDRESS).deploy{value: totalFee}(
-            deployByteCode, gasLimit, salt, isUniquePerChain, constructorArgs, initDatas, destinationDomainIDs, fees
+            deployByteCode, gasLimit, salt, isUniquePerChain, constructorArgs, initDatas, _deploymentNetworks, fees
         );
     }
+
+    /**
+     * This function willl take the network, constructor args and initdata and
+     * save these to a mapping (what type?)
+     */
+    function addDeploymentTarget(string deploymentTarget) public {
+        uint8 deploymentTargetDomainId = _stringToDeploymentNetwork[deploymentTarget];
+        require(deploymentTargetDomainId != 0, "Invalid deployment target");
+        _deploymentNetworks.push(deploymentTargetDomainId);
+        // TODO: what else does this need to do?
+    }
+
+    // what does this do?
+    function encodeInitData() {}
+
+    // what does this do?
+    function generateSalt() {}
+
+    // what does this do?
+    function getDeploymentFee() {}
+
+    // what does this do?
+    function computeAddressForChain() {}
 }
